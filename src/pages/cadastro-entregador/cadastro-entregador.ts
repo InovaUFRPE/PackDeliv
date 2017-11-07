42c3f986f7f06e923cc28165326bbb6d45c02f0e
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2017-11-07 10:53:13 -0300

    Passa as informações do usuário da tela de cadastro para a tela de cadastro de veículo, no caso de cadastro de entregador.

--- src/pages/cadastro-entregador/cadastro-entregador.ts
+++ src/pages/cadastro-entregador/cadastro-entregador.ts
@@ -20,10 +20,28 @@ export class CadastroEntregadorPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CadastroEntregadorPage');
-  }
-  irParaCadastroVeiculo(){
-    this.navCtrl.push(CadastroVeiculoPage)
+  /**
+   * Vai para a tela de cadastro de veículo,
+   * passando as informações do usuário para a próxima tela.
+   * 
+   * Feito por: Matheus Campos da Silva, 07/11/2017
+   */
+  public irParaCadastroVeiculo(){
+    // Pega as informações do formulário
+    var nomeUsuario: string = (<HTMLInputElement>document.getElementById('inputNomeUsuario')).value;
+    var nomeCompleto: string = (<HTMLInputElement>document.getElementById('inputNomeCompleto')).value;
+    var cnpj: string = (<HTMLInputElement>document.getElementById('inputCNPJ')).value;
+    var cnh: string = (<HTMLInputElement>document.getElementById('inputCNH')).value;
+
+    // Cria o objeto usuario
+    var usuario: object = {
+      nomeUsuario: nomeUsuario,
+      nomeCompleto: nomeCompleto,
+      cnpj: cnpj,
+      cnh: cnh
+    };
+
+    // Passa o objeto usuario para a tela de cadastro de veículo
+    this.navCtrl.push(CadastroVeiculoPage, usuario);
   }
 }
