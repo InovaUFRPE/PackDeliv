784dc2186fd8ac398dd8a3e70ca2f8577dd5d1a3
Filipe Carlos <filcarx@gmail.com>
2018-01-11 17:27:17 +0000

    Criando Ordem de Serviço "temporária" em Servidor_Calc

--- Servidor_Calc/app/controller/combination.py
+++ Servidor_Calc/app/controller/combination.py
@@ -2,6 +2,8 @@
 """Combination Controller module, create the controller for combination."""
 
 from app.dao.combination import CombinationDAO
+from app.lib.utils import ServiceOrder
+from datetime import datetime
 
 combinationDAO = CombinationDAO()
 
@@ -13,7 +15,7 @@ class CombinationController:
         """Method responsible for the match of the packages."""
         packages = combinationDAO.select_packages(vehicle['position'])
         # service_order = Service_order()
-        pack_list = {}
+        pack_list = []
         available_vol = 0.8 * vehicle['vol']
         available_weight = 0.8 * vehicle['weight']
 
@@ -24,9 +26,12 @@ class CombinationController:
             if (package.vol <= available_vol) and (package.weight <= available_weight):
                 available_vol -= package.vol
                 available_weight -= package.weight
-                pack_list[package.id] = package.get()
+                pack_list.append(package.get())
             count += 1
-
+        dateNow = datetime.now()
+        shipping_date = str(dateNow.day) + "/" + str(dateNow.month) + "/" + str(dateNow.year)
+        finalization_date = str(dateNow.day + 2) + "/" + str(dateNow.month) + "/" + str(dateNow.year)
+        service_order = ServiceOrder(pack_list, shipping_date, finalization_date)
         # service_order.list_package = pack_list
         # service_order.shipping_date = datetime.datetime.today()
-        return pack_list
+        return service_order.get()
